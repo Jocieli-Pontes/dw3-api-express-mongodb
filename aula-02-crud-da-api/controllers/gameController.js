5a52cb7da99dcdf1e8e7aba3b3181f9b034b1d62
Jocieli Pontes <jocielipdomingues@gmail.com>
2026-08-18 20:05:41 -0300

    aula 02 crud da api

---
+++ aula-02-crud-da-api/controllers/gameController.js
@@ -0,0 +1,35 @@
+// Controller de games
+// O controller tratará as requisições do cliente
+// Importando o service
+import gameService from '../services/gameService.js';
+
+// Função que irá tratar a requisição para LISTAR os jogos
+const getAllGames = async (req, res) => {
+    try {
+        const games = await gameService.getAll()
+        res.status(200).json({ games: games })
+        // Cod. 200 - OK - Requisição feita com sucesso
+    } catch (error) {
+        console.log(error)
+        // Tratando a resposta que api irá enviar em caso de erro
+        res.status(500).json({ error: 'Ocorreu um erro ao listar os jogos. Erro interno do servidor.' })
+    }
+}
+//função que ira tratar a requisição da cadastrar os jogos
+const createGame =async(req, res)=>{
+    try{
+        //const title = req.body.title
+        //const yaer = req.body.yaer
+        //coletando dados enviados (formulario, da requisição, etc) e gravando nas variaveis
+        const{title, year, platform, price}= req.body
+        //enviando dadospara o Service cadastrar
+        await gameService.Create(title, year,platform,price)
+        res.status(201).json({message: "Jogo cadastrado com sucesso!"})
+        //Cod. 201 (create) --> recurso criado com sucesso no servidor
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:"Erro interno do servidor."});
+    }
+}
+// Exportando as funções
+export default { getAllGames,createGame}
